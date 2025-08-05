0e3ddc3bb899ee448173953863d20560c582ab20
Vladimir Kondrashov <vova.kondrashov1993@gmail.com>
2025-08-05 20:17:29 +0300

    upd

--- study/leetcode/26_remove_duplicates.py
+++ study/leetcode/26_remove_duplicates.py
@@ -19,35 +19,19 @@ j — ищет новые уникальные элементы.
 Мы сканируем уникальную часть на уникальные элементы.
 """
 
+nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
 
+if not nums:
+    print(0, nums)
+else:
+    i = 0
+    for j in range(1, len(nums)):
+        if nums[j] != nums[i]:
+            i += 1
+            nums[i] = nums[j]
 
-# nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
-#
-# if not nums:
-#     print(0, nums)
-# else:
-#     i = 0
-#     for j in range(1, len(nums)):
-#         if nums[j] != nums[i]:
-#             i += 1
-#             nums[i] = nums[j]
-#
-#     # Заполняем хвост '_'
-#     for k in range(i + 1, len(nums)):
-#         nums[k] = '_'
-#
-#     print(i + 1, nums)
+    # Заполняем хвост '_'
+    for k in range(i + 1, len(nums)):
+        nums[k] = '_'
 
-
-def upd_dict(*args, **kwargs):
-    s = dict()
-
-    # Обрабатываем args (например, добавляем их как ключи с None-значениями)
-    for i, arg in enumerate(args):
-        s[f"arg_{i}"] = arg
-
-    s.update(kwargs)
-
-    return s
-
-print(upd_dict('sdr', a=1, b=2, c=3, d=4))
+    print(i + 1, nums)
