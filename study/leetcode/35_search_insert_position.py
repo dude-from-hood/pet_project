46be07437e3b15b56f116752d75ba42f76e652cb
Vladimir Kondrashov <vova.kondrashov1993@gmail.com>
2025-08-06 20:57:25 +0300

    add 35 leetcode task

---
+++ study/leetcode/35_search_insert_position.py
@@ -0,0 +1,35 @@
+"""
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+
+######
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+"""
+from typing import List
+
+
+class Solution:
+    def search_insert(self, nums: List[int], target: int) -> int:
+        if target in nums:
+            pos = nums.index(target)
+            return pos
+        else:
+            i = 0
+            if target > nums[-1]:
+                return nums.index(nums[-1]) + 1
+
+            for j in range(len(nums)):
+                if target > nums[j]:
+                    i += 1
+                    continue
+                else:
+                    return i
+
+
+nums = [1, 3, 5, 6]
+target = 5
+
+s = Solution()
+
+print(s.search_insert(nums=nums, target=target))
