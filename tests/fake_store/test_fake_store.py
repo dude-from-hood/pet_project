5574a501696ec570f4e3838fbeca8127416513a9
Vladimir Kondrashov <vova.kondrashov1993@gmail.com>
2025-12-27 18:07:10 +0300

    добавлен докерфайл

--- tests/fake_store/test_fake_store.py
+++ tests/fake_store/test_fake_store.py
@@ -1,22 +1,15 @@
 import json
 import time
-
+import pprint
 import allure
 
 from common.bindings.fake_market.controller import FakeStoreController
-import pprint
-
 
 class TestCrudFakeStore:
 
     def test_create_new_product(self, data_test_create_product):
         # Сохраняем данные в локальный файл для отладки
-        from datetime import datetime
         time.sleep(1)
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        debug_filepath = f"debug_test_data_{timestamp}.json"
-        with open(debug_filepath, "w", encoding="utf-8") as f:
-            json.dump(data_test_create_product, f, indent=4, default=str, ensure_ascii=False)
 
         test_product = data_test_create_product['test_data']
         allure.attach(json.dumps(data_test_create_product, indent=4, default=str, ensure_ascii=False),
@@ -27,7 +20,7 @@ class TestCrudFakeStore:
         )
         response_json = response.json()
 
-        assert 203 == response.status_code
+        assert 201 == response.status_code
 
         for key, value in test_product.items():
             if key == 'id':
