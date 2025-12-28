38fb87dc1a8b8556aee556cd6a7e91e0e6b2d79b
Vladimir Kondrashov <vova.kondrashov1993@gmail.com>
2025-12-28 16:26:29 +0300

    апд sorted

--- study/tasks/most_popular_city.py
+++ study/tasks/most_popular_city.py
@@ -1,15 +1,13 @@
 if __name__ == '__main__':
     from collections import Counter
-    most_popular_city = ["New York, USA", "London, UK",
-                         "New York, USA", "Paris, France",
-                         "London, UK", "New York, USA"]
+    most_popular_city = ["London, UK", "Paris, France", "Berlin, Germany", "Paris, France", "London, UK"]
 
     # Вариант №1
     #Counter({'New York, USA': 3, 'London, UK': 2, 'Paris, France': 1})
-    obj = Counter(most_popular_city)
-    #самый первый элемент - [('New York, USA', 3)]
-    pop = obj.most_common(1)[0][0]
-    print(pop.split(",")[0])
+    # obj = Counter(most_popular_city)
+    # #самый первый элемент - [('New York, USA', 3)]
+    # pop = obj.most_common(1)[0][0]
+    # print(pop.split(",")[0])
 
     # Вариант №2
     set_cities = set(most_popular_city)
@@ -22,7 +20,8 @@ if __name__ == '__main__':
         if item in dict_cities.keys():
             dict_cities[item] += 1
 
-    sort_ = sorted(dict_cities.items(), key=lambda x: x[1], reverse=True)
+    #для сортировки по убыванию по кол-ву упоминаний ставим -x[1]
+    sort_ = sorted(dict_cities.items(), key=lambda x: (-x[1], x[0]))
     most_popular = str(sort_[0][0])
     print(most_popular.split(',')[0])
 
