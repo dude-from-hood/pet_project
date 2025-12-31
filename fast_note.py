400da5f7aab4f39046f52388dd053b359f59a761
Vladimir Kondrashov <vova.kondrashov1993@gmail.com>
2025-12-31 11:54:39 +0300

    upd2

--- fast_note.py
+++ fast_note.py
@@ -1,3 +1,27 @@
-if __name__ == '__main__':
-    ...
+import time
+from functools import wraps
 
+
+def execution_time_deco(func):
+    @wraps(func)  # создаем декоратор
+    def wrapper():
+        print("Функция начинает свою работу!")
+        start = time.time()          # Замер до вызова
+        result = func()              # Выполняем функцию
+        finish = time.time()         # Замер после вызова
+        duration = finish - start
+        print(f"Время выполнения: {duration:.6f} сек")
+        print("Функция закончила свою работу")
+        return result                # Возвращаем результат функции, а не время
+    return wrapper
+
+@execution_time_deco
+def my_func():
+    """важный docstring"""
+    s = [i for i in range(1000000)]  # пример полезной работы
+    return "Готово!"
+
+my_func()
+
+print(my_func.__name__)
+print(my_func.__doc__)
