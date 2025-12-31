c85725fecab31e7cb414a49ff957e6e23307ce29
Vladimir Kondrashov <vova.kondrashov1993@gmail.com>
2025-12-31 11:49:29 +0300

    поменял базовый пример для декоратора

--- study/decorator/basic.py
+++ study/decorator/basic.py
@@ -1,198 +1,27 @@
-"""
-Декоратор — это функция, которая позволяет обогатить любую другую функцию дополнительным поведением
-    без необходимости изменения ее кода.
-Декоратор очень часто называют оберткой для функции, потому что он оборачивает ее
-    и позволяет добавить новый функционал как перед вызовом функции, так и после завершения ее работы.
-
-Чтобы создать декоратор, необходимо определить внешнюю функцию, которая будет принимать функцию в качестве аргумента.
-Затем внутри внешней функции необходимо создать внутреннюю функцию, в которой будет происходить вызов декорированной функции.
-
-После уже можно повестить тег на функцию с именем обертки (декоратора) при вызове, порядок исполнения см.ниже:
-"""
-from functools import wraps
-from typing import Callable
 import time
+from functools import wraps
 
-from common.bindings.utils.create_logger import setup_logger
 
-#+=================================
-# Константы
-logger = setup_logger()
-#+=================================
-
-#Задача №1: "Волшебный приветственник"
-def my_deco(func: Callable):  # создаем декоратор
+def execution_time_deco(func):
+    @wraps(func)  # создаем декоратор
     def wrapper():
         print("Функция начинает свою работу!")
-        res = func()
-        return res
-
+        start = time.time()          # Замер до вызова
+        result = func()              # Выполняем функцию
+        finish = time.time()         # Замер после вызова
+        duration = finish - start
+        print(f"Время выполнения: {duration:.6f} сек")
+        print("Функция закончила свою работу")
+        return result                # Возвращаем результат функции, а не время
     return wrapper
 
+@execution_time_deco
+def my_func():
+    """важный docstring"""
+    s = [i for i in range(1000000)]  # пример полезной работы
+    return "Готово!"
 
-# @my_deco
-# def my_func():
-#     print("Привет, я функция!")
-#
-#
-# my_func()
-# print('=' * 50)
-#
-#
-# #+-------------------------------------
-# #Задача №2: функция c параметрами
-# def my_deco_func_params(func: Callable):  # создаем декоратор
-#     def wrapper(*args, **kwargs):
-#         res = func(*args, **kwargs)
-#         return res
-#
-#     return wrapper
-#
-#
-# @my_deco_func_params
-# def greet(name, surname):
-#     print(f"Привет, {name} {surname}!")
-#
-#
-# greet("Anna", "Smith")
-# print('=' * 50)
-#
-#
-# #+-------------------------------------
-# #Задача №3: "Декоратор-секундомер"
-# #создаем декоратор-функцию
-# def timer(func: Callable):
-#     def wrapper(*args, **kwargs):
-#         print("Засекаем время...")
-#         start = time.time()
-#         res = func(*args, **kwargs)
-#         end = time.time()
-#         print(f"Функция выполнялась {end - start} секунд")
-#         return res
-#
-#     return wrapper
-#
-#
-# #вешаем декоратор на нужную функцию
-# @timer
-# def slow_function(seconds):
-#     time.sleep(seconds)
-#     print(f"Я поспал {seconds} секунд")
-#
-#
-# #slow_function(2)
-# print('=' * 50)
+my_func()
 
-
-#+-------------------------------------
-#Задача №4: "Умный декоратор с аргументами"
-# def deco_repeat(n: int):
-#     # здесь нужно вернуть декоратор
-#     def wrapper(func: Callable):
-#         def inner_wrapper(*args, **kwargs):
-#             for i in range(n):
-#                 func(*args, **kwargs)
-#
-#         return inner_wrapper
-#
-#     return wrapper
-#
-#
-# @deco_repeat(3)
-# def say_hello():
-#     print("Привет!")
-#
-#
-# say_hello()
-# print('=' * 50)
-#
-
-#+-------------------------------------
-
-#Задача №5: "Декоратор-ограничитель"
-def limit_calls(limit: int):
-    def wrapper(func: Callable):
-        # сохранить метданные функции - __doc__, __name__
-        @wraps(func)
-        def inner_wrapper(*args, **kwargs):
-            """Хехехе докстринг"""
-
-            # переменная принадлежит не локальной области видимости этой функции, а ближайшей охватывающей (внешней) функции
-            # имя такое же как во внешней функции
-            nonlocal limit
-            if limit == 0:
-                print("Превышен лимит вызовов!")
-                return
-
-            res = func(*args, **kwargs)
-            limit -= 1
-            return res
-
-        return inner_wrapper
-
-    return wrapper
-
-
-@limit_calls(3)
-def say_hello():
-    """Очень важный докстинг не потерять его!!!""" # вешаем декоратор @wraps(func) на inner_wrapper
-    print("Привет!")
-
-print(say_hello.__name__)
-print(say_hello.__doc__)
-say_hello()  # Работает
-say_hello()  # Работает
-say_hello()  # Превышен лимит
-say_hello()  # Превышен лимит
-print('=' * 50)
-
-
-#+-------------------------------------
-
-#Задача №6: "Умный декоратор с логированием и проверкой"
-# Настраиваем базовую конфигурацию логирования
-#
-# def deco_logged(level: str):
-#     # Проверяем валидность уровня
-#     if level not in ('info', 'debug'):
-#         raise ValueError("Уровень должен быть 'info' или 'debug'")
-#
-#     def wrapper(func: Callable):
-#         def inner_wrapper(*args, **kwargs):
-#             # Логируем ДО вызова функции
-#             if level == "info":
-#                 logger.info(f"Вызов функции {func.__name__}")
-#             elif level == "debug":
-#                 # Формируем строку с аргументами
-#                 args_str = ", ".join([repr(arg) for arg in args])
-#                 kwargs_str = ", ".join([f"{key}={repr(value)}" for key, value in kwargs.items()])
-#                 all_args = ", ".join(filter(None, [args_str, kwargs_str]))
-#                 logger.debug(f"Вызов функции {func.__name__} с аргументами: {all_args}")
-#
-#             # Вызываем функцию и возвращаем результат
-#             return func(*args, **kwargs)
-#
-#         return inner_wrapper
-#
-#     return wrapper
-#
-#
-# @deco_logged('info')
-# def add(a, b):
-#     return a + b
-#
-#
-# @deco_logged('debug')
-# def greet(name, age=None):
-#     if age:
-#         return f"Привет, {name}! Тебе {age} лет."
-#     return f"Привет, {name}!"
-#
-#
-# result1 = add(5, 3)
-# result2 = greet("Анна")
-# result3 = greet("Петр", age=25)
-#
-# print(result1)
-# print(result2)
-# print(result3)
+print(my_func.__name__)
+print(my_func.__doc__)
