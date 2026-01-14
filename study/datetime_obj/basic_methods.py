78fd94bbea411dce88d9eb92cdedb6cd03202869
Vladimir Kondrashov <vova.kondrashov1993@gmail.com>
2026-01-14 22:43:18 +0300

    datetime obj

---
+++ study/datetime_obj/basic_methods.py
@@ -0,0 +1,28 @@
+if __name__ == '__main__':
+    from datetime import datetime, timedelta
+    import pytz
+
+    # задать часовой пояс
+    msk_tz = pytz.timezone('Europe/Moscow')
+
+    # текущее время в Москве - datetime
+    time_msk = datetime.now(tz=msk_tz)
+    print(time_msk)
+
+    # время в UTC - datetime
+    time_utc = time_msk.astimezone(tz=pytz.UTC)
+    print(time_utc)
+
+    # время в UTC - str на конце Z
+    time_utc_with_z = time_utc.strftime('%Y-%m-%dT%H:%M:%SZ')
+    print(time_utc_with_z)
+
+    # unix
+    unix_time = int(time_msk.timestamp())
+    print(unix_time)
+
+    # Обратное преобразование: timestamp → aware datetime в UTC, затем в Москву
+    dt_utc = datetime.fromtimestamp(unix_time, tz=pytz.UTC)
+    print(dt_utc)
+    dt_moscow_roundtrip = dt_utc.astimezone(msk_tz)
+    print(dt_moscow_roundtrip)
