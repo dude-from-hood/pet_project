69ce691d57d385e3f558c8ff4e6f059cfff6141f
Vladimir Kondrashov <vova.kondrashov1993@gmail.com>
2026-01-24 17:17:12 +0300

    обновил конфиг логов (макс просто)

--- common/bindings/utils/create_logger.py
+++ common/bindings/utils/create_logger.py
@@ -1,38 +1,10 @@
+# log.py
 import logging
-import sys
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(funcName)s:%(lineno)d - %(message)s',
+    force=True
+)
 
-def setup_logger(name=__name__, level=logging.DEBUG):
-    """
-    Настраивает и возвращает логгер с форматом
-
-    Args:
-        name: имя логгера (обычно __name__)
-        level: уровень логирования (по умолчанию DEBUG)
-
-    Returns:
-        Настроенный логгер
-    """
-    # Создаем логгер
-    logger = logging.getLogger(name)
-    logger.setLevel(level)
-
-    # Проверяем, нет ли уже обработчиков (чтобы избежать дублирования)
-    if logger.handlers:
-        logger.handlers.clear()
-
-    # Создаем обработчик для консоли
-    console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setLevel(level)
-
-    # Создаем красивый форматтер
-    formatter = logging.Formatter(
-        '%(asctime)s | %(levelname)-8s | %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S'
-    )
-    console_handler.setFormatter(formatter)
-
-    # Добавляем обработчик к логгеру
-    logger.addHandler(console_handler)
-
-    return logger
+log = logging.getLogger("myapp")
--- study/logging/base_example.py
+++ study/logging/base_example.py
@@ -1,16 +1,13 @@
-from common.bindings.utils.create_logger import setup_logger
-
-# Использование
-logger = setup_logger(__name__)
+from common.bindings.utils.create_logger import log
 
 def divide_numbers(a, b):
-    logger.info(f"Вызвана функция divide_numbers с аргументами: a={a}, b={b}")
+    log.info(f"Вызвана функция divide_numbers с аргументами: a={a}, b={b}")
     try:
         result = a / b
-        logger.debug(f"Результат деления: {result}")
+        log.info(f"Результат деления: {result}")
         return result
     except ZeroDivisionError:
-        logger.error("Попытка деления на ноль!", exc_info=True) # exc_info=True добавит traceback
+        log.error("Попытка деления на ноль!", exc_info=True) # exc_info=True добавит traceback
         return None
 
 # Использование
