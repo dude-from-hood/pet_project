4b3ce977cc1c6439ed19ad5003c566b613047cbf
Vladimir Kondrashov <vova.kondrashov1993@gmail.com>
2026-03-29 21:14:13 +0300

    upd тесты BA

--- study/property/bank_account.py
+++ study/property/bank_account.py
@@ -3,17 +3,18 @@ import pytest
 
 class BankAccount:
 
-    def __init__(self,
-                 account_number: str,
-                 initial_balance: float
-                 ):
+    def __init__(
+        self,
+        account_number: str,
+        initial_balance: float
+    ):
 
         if not len(account_number) >= 5:
             raise ValueError("account_number should be >= 5")
         if not account_number.isdigit():
             raise ValueError("account_number should be string of digits")
-        self.__account_number = account_number
 
+        self.__account_number = account_number
 
         if not isinstance(initial_balance, (int, float)):
             raise ValueError("initial_balance should be int or float")
@@ -46,8 +47,33 @@ class BankAccount:
     def __str__(self):
         return f"BankAccount(acc={self.account_number}, balance={self.balance})"
 
+class LimitedWithdrawAccount(BankAccount):
 
-class TestBankAccount:
+    def __init__(
+        self,
+        account_number: str,
+        initial_balance: float,
+        max_withdrawal: float,
+    ):
+        # вызываем конструктор родителя
+        super().__init__(account_number, initial_balance)
+
+        if not isinstance(max_withdrawal, (int, float)):
+            raise ValueError("max_withdrawal must be a number")
+        if max_withdrawal <= 0:
+            raise ValueError("max_withdrawal must be positive")
+
+        # инициализируем новый параметр
+        self.__max_withdrawal = max_withdrawal
+
+
+    def withdraw(self, amount):
+        if not amount <= self.__max_withdrawal:
+            raise ValueError("withdrawal exceeds max limit")
+
+        super().withdraw(amount)
+
+class TestAccount:
 
     @pytest.mark.parametrize("acc, balance", [
         ('12345', 100),
@@ -61,7 +87,6 @@ class TestBankAccount:
         assert ba.balance == balance
         assert isinstance(ba.balance, (int, float))
 
-
     @pytest.mark.parametrize("acc_num, balance", [
         ("1234", 100),  # короткий номер
         ("abcde", 100),  # не цифры
@@ -73,4 +98,63 @@ class TestBankAccount:
         with pytest.raises(ValueError):
             BankAccount(acc_num, balance)
 
-    # ввести наследование, использовать super.init()
+    #========== Вариант через фикстуру =========
+
+    # фикстура валидных тест-кейсов
+    @pytest.fixture(params=[
+        ('12345', 100),
+        ('123456', 1000.00),
+        ('12345', 1),
+        ('12345', 0),
+    ])
+    def create_valid_cases(self, request):
+        return request.param
+
+    # тест
+    def test_valid_cases(self, create_valid_cases):
+        # распаковываем кортежи
+        acc, balance = create_valid_cases
+        # создаем объект
+        ba = BankAccount(acc, balance)
+
+        # делаем проверки
+        assert ba.account_number == acc
+        assert ba.balance == balance
+        assert isinstance(ba.balance, (int, float))
+
+
+    @pytest.fixture(params=[
+        ('12345', 100, 30, 10, 90), # снимаем 10, получаем баланс 90
+    ])
+    def create_positive_cases_max_withdrawal(self, request):
+        return request.param
+
+    def test_max_withdrawal_positive(self, create_positive_cases_max_withdrawal):
+
+        acc, balance, max_withdrawal, withdrawal, final_balance = create_positive_cases_max_withdrawal
+        obj = LimitedWithdrawAccount(acc, balance, max_withdrawal)
+
+        obj.withdraw(withdrawal)
+
+        print(f"\n{obj.balance}")
+
+        assert obj.account_number == acc
+        assert obj.balance == final_balance
+
+
+    @pytest.mark.parametrize("acc, balance, max_wd, amount", [
+        ("22222", 100, 50, 60),  # превышение лимита при снятии
+    ])
+    def test_withdraw_exceeds_limit(self, acc, balance, max_wd, amount):
+        acc_obj = LimitedWithdrawAccount(acc, balance, max_wd)
+        with pytest.raises(ValueError, match="exceeds max limit"):
+            acc_obj.withdraw(amount)
+
+
+    @pytest.mark.parametrize("acc, balance, max_wd", [
+        ("33333", 100, "2"),  # неверный тип max_withdrawal
+    ])
+    def test_invalid_max_withdrawal_type(self, acc, balance, max_wd):
+        with pytest.raises(ValueError, match="must be a number"):
+            LimitedWithdrawAccount(acc, balance, max_wd)
+
