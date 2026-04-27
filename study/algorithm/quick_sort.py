48b23ade9b9638472b5f8bb57f54b463ddeb2e5d
Vladimir Kondrashov <vova.kondrashov1993@gmail.com>
2026-04-27 16:14:38 +0400

    Простая реализация быстрой сортировки Хоара

---
+++ study/algorithm/quick_sort.py
@@ -0,0 +1,26 @@
+import random
+
+def QuickSort(A: list):
+    if len(A) <= 1:
+        return A
+    else:
+        pivot = random.choice(A)
+        L = []
+        M = []
+        R = []
+        for elem in A:
+            if elem < pivot:
+                L.append(elem)
+            elif elem > pivot:
+                R.append(elem)
+            else:
+                M.append(elem)
+        return QuickSort(L) + M + QuickSort(R)
+
+
+some_list = [random.randint(1, 9) for _ in range(10)]
+
+print(some_list)
+print("*** магия сортировки ***")
+print(QuickSort(some_list))
+
